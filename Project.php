f19393d345f45bd3b386e8a98032aea76c02c355
Rifqi Ardiansyah Wijaya <44166565+ardiansyahwijaya@users.noreply.github.com>
2018-11-05 11:36:17 +0700

    Initial commit

---
+++ Project.php
@@ -0,0 +1,12 @@
+<?php
+	echo "Hello Fans-Kuh [DheaQ]";
+
+	//ini komentar cara 1, boleh ndak ditutup
+
+	#ini komentar cara 2, boleh ndak ditutup
+
+	/*ini komentar cara 3, harus ditutup*/
+
+	$ //fungsinya buat nyimpen variabel
+	echo ; //fungsinya buat manggil atau nampilin output
+?>
---
+++ Tugas.php
@@ -0,0 +1,16 @@
+<?php
+	$nama = "Dhea Qonita Irawan";
+	$kelas = "XI RPL 4";
+	$result = mysql_connect('localhost','username','password')
+	$x = 2 //tipe data integeer
+	$y = 0.5 //tipe data float
+	$a = 'kata' //tipe data string
+	$b = TRUE //tipe data boolean
+	$arr = array('5' => 11,12,13,14,15); //tipe data array
+	$o = new object // object nya itu class baru
+ 
+	echo "Hellow Fanskuh <3";
+	echo "Namaku	: ". $nama ."<br>";
+	echo "Kelasku	: ". $kelas;
+
+?>
